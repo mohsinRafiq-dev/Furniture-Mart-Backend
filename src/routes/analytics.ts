bc6997b9a41ee8a00174e824ad17eeb308af47e3
Furniture Mart Developer <dev@furniture-mart.com>
2025-11-28 12:16:25 +0500

    fixes

--- src/routes/analytics.ts
+++ src/routes/analytics.ts
@@ -1,5 +1,5 @@
 import { Router, Request, Response } from "express";
-import { AnalyticsService } from "../utils/AnalyticsService";
+import { AnalyticsService } from "../utils/AnalyticsService.js";
 
 const router = Router();
 
